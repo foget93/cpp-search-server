18499dbfcc28df0fc28bf75612e1da30a46cb58e
Dmitrii Makarov <dmitriymakarov93@gmail.com>
2022-07-11 10:09:27 +0300

    first commit to the project

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,17 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
+#include <iostream>
+#include <string>
 
+int main()
+{
+    int count {0};
+    for (int i = 1; i <= 1000; ++i) {
+        if (std::to_string(i).find('3') != std::string::npos)
+            count++;
+    }
+    std::cout << "kol-vo vhojdeniy 3 : " << count << std::endl;
+
+    return 0;
+}
 // Закомитьте изменения и отправьте их в свой репозиторий.
